48f2a0f81d88c9e9a67dfe97f991ca0808f27562
Noarkhh <66123336+Noarkhh@users.noreply.github.com>
2022-03-15 02:05:31 +0100

    added roads and road snapping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import os
 from pygame.locals import (RLEACCEL,
                            K_UP,
                            K_DOWN,
@@ -9,11 +10,14 @@ from pygame.locals import (RLEACCEL,
                            QUIT,
                            K_ESCAPE,
                            K_t,
-                           K_h)
+                           K_h,
+                           K_r,
+                           K_n)
 
 HEIGHT_TILES = 12
 WIDTH_TILES = 18
 TILE_SIZE = 60
+TICK_RATE = 20
 
 
 class Cursor(pg.sprite.Sprite):
@@ -21,7 +25,7 @@ class Cursor(pg.sprite.Sprite):
         super().__init__()
         self.windup = [0 for _ in range(4)]
         self.cooldown = [0 for _ in range(4)]
-        self.surf = pg.image.load("assets/cursor2.png").convert()
+        self.surf = pg.image.load("assets/cursor3.png").convert()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect()
         self.position = [0, 0]
@@ -103,6 +107,39 @@ class Tower(Structure):
         self.surf.set_colorkey((0, 0, 0), RLEACCEL)
 
 
+class Road(Structure):
+    def __init__(self, xy):
+        super().__init__(xy)
+        self.neighbours = set()
+        self.surf = pg.transform.scale(pg.image.load("assets/roads/road0.png").convert(), (60, 60))
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+
+    def update_edges(self, direction, roads_list):
+        self.neighbours.add(direction)
+        print(self.neighbours)
+
+        def assign_value(direct):
+            if direct == 'N': return 0
+            if direct == 'E': return 1
+            if direct == 'S': return 2
+            if direct == 'W': return 3
+
+        directions = tuple(sorted(self.neighbours, key=assign_value))
+
+        self.surf = roads_list[directions]
+
+
+def fill_roads_list(roads_list):
+    directory = os.listdir("assets/roads")
+    dir_cut = []
+    for name in directory:
+        dir_cut.append(tuple(name[4:-4]))
+
+    for file, name in zip(directory, dir_cut):
+        roads_list[name] = pg.transform.scale(pg.image.load("assets/roads/" + file).convert(), (60, 60))
+        roads_list[name].set_colorkey((255, 255, 255), RLEACCEL)
+
+
 if __name__ == "__main__":
     pg.init()
     pg.mixer.init()
@@ -114,17 +151,25 @@ if __name__ == "__main__":
     cursor = Cursor()
     game_board = [[0 for _ in range(HEIGHT_TILES)] for _ in range(WIDTH_TILES)]
 
+    directions_list = [('0',), ('N',), ('E',), ('S',), ('W',), ('N', 'E'), ('E', 'S'), ('S', 'W'), ('N', 'W'),
+                       ('N', 'S'), ('E', 'W'), ('N', 'E', 'S'), ('E', 'S', 'W'),
+                       ('N', 'S', 'W'), ('N', 'E', 'W'), ('N', 'E', 'S', 'W')]
+    roads_list = {directions_list[i]: pg.Surface((60, 60)) for i in range(16)}
+    # print(directions_list)
+    fill_roads_list(roads_list)
+
     background = pg.image.load("assets/background.png").convert()
     houses = pg.sprite.Group()
     towers = pg.sprite.Group()
+    roads = pg.sprite.Group()
     structures = pg.sprite.Group()
     all_sprites = pg.sprite.Group()
     all_sprites.add(cursor)
 
     clock = pg.time.Clock()
-    key_structure_dict = {K_h: House, K_t: Tower}
-    structure_group_dict = {House: houses, Tower: towers}
-
+    key_structure_dict = {K_h: House, K_t: Tower, K_r: Road}
+    structure_group_dict = {House: houses, Tower: towers, Road: roads}
+    # i = 0
     running = True
     # ------ MAIN LOOP -------
     while running:
@@ -139,7 +184,8 @@ if __name__ == "__main__":
                     cursor.holding = key_structure_dict[event.key]([0, 0])
                     structure_ghost = Ghost(cursor.position, cursor.holding.surf)
                     violin_se.play()
-
+                if event.key == K_n:
+                    cursor.holding = None
                 if event.key == K_SPACE:  # placing down held structure
                     if isinstance(cursor.holding, Structure):
                         if game_board[cursor.position[0]][cursor.position[1]] not in structures:
@@ -149,9 +195,19 @@ if __name__ == "__main__":
                             all_sprites.add(new_structure)
                             game_board[new_structure.position[0]][new_structure.position[1]] = new_structure
                             boom_se.play()
-                            cursor.holding = None
+
+                            if isinstance(new_structure, Road):
+                                for direction, direction_rev, x, y in zip(('N', 'E', 'S', 'W'), ('S', 'W', 'N', 'E'),
+                                                          (0, -1, 0, 1), (1, 0, -1, 0)):
+                                    if isinstance(game_board[cursor.position[0] + x][cursor.position[1] + y], Road):
+                                        game_board[cursor.position[0] + x][cursor.position[1] + y].\
+                                                   update_edges(direction, roads_list)
+                                        new_structure.update_edges(direction_rev, roads_list)
+                            else:
+                                cursor.holding = None
                         else:
                             bruh_se.play()
+
                 if event.key == K_ESCAPE:
                     running = False
 
@@ -167,6 +223,9 @@ if __name__ == "__main__":
             screen.blit(structure_ghost.surf, structure_ghost.rect)
         screen.blit(cursor.surf, cursor.rect)
 
+        # screen.blit(roads_list[directions_list[i]], (180, 180))
+        # i += 1
+
         pg.display.flip()
-        clock.tick(20)
+        clock.tick(TICK_RATE)
     pg.quit()
